4b84640d4f789ea9f000661aec5523224c727ada
sorc <sorc@qq.com>
2025-12-31 08:34:14 +0800

    更新java版本

--- boothelper/docker-build.sh
+++ boothelper/docker-build.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 name=boothelper
-#openjdk:8u342-jdk-bullseye 225.98MB
 #openjdk:8u342-slim-bullseye 132.69MB
 
-#openjdk:11-jdk-bullseye 317.68MB
-#openjdk:11-slim-bullseye
+#openjdk:26-ea-11-slim 243.52 MB
 
-#openjdk:22-ea-17-jdk-bookworm
-#openjdk:17-jdk-bullseye 312.09MB
-#openjdk:17-slim-bullseye 210.63MB
+#openjdk:26-ea-17-slim 245.96 MB
 
-#openjdk:22-ea-21-jdk-bookworm
-#openjdk:21-jdk-bookworm
-#openjdk:21-slim-bookworm
-#java_ver=17-slim-bullseye
+#openjdk:26-ea-21-slim 246.1 MB
+
+#openjdk:26-ea-25-slim 248.11 MB
+
+java_ver=$1
+if [ -z "${java_ver}" ] ;then
+  echo "please input java version, example: 26-ea-25-slim"
+  exit 1
+fi
 build_date=$(date +"%Y%m%d")
 full_version=${java_ver}-${build_date}
 echo ${full_version}
-#登录镜像仓库
-echo ${DOCKER_HUB_KEY} | docker login --username ${DOCKER_HUB_USER} --password-stdin
+export DOCKER_CLI_EXPERIMENTAL=enabled
+export DOCKER_BUILDKIT=1
+
 docker buildx build \
   --platform linux/amd64,linux/arm64 \
   --build-arg JAVA_VER=${java_ver} \
   --build-arg FULL_VERSION=${full_version} \
-  --build-arg HTTP_PROXY=http://10.10.10.41:1083 \
-  --build-arg HTTPS_PROXY=http://10.10.10.41:1083 \
   --push \
   --tag sorc/${name}:${full_version} \
   --tag sorc/${name}:${java_ver} .
